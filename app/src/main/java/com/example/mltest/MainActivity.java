038ea783654055f051c43500bdab462f2913e6a8
2020akumar <aka.kumario2002@gmail.com>
2019-09-26 11:59:07 -0400

    update

--- app/src/main/java/com/example/mltest/MainActivity.java
+++ app/src/main/java/com/example/mltest/MainActivity.java
@@ -1,14 +1,31 @@
 package com.example.mltest;
 
 import androidx.appcompat.app.AppCompatActivity;
+import com.chaquo.python.Kwarg;
+import com.chaquo.python.PyObject;
+import com.chaquo.python.Python;
+import com.chaquo.python.android.AndroidPlatform;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Python py;
+    private Button mButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Python.start(new AndroidPlatform(this));
+        py=Python.getInstance();
+//        py.getModule("main").callAttr("main");
+        mButton=(Button)findViewById(R.id.button);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                py.getModule("main").callAttr("main");
+            }
+        });
     }
 }
