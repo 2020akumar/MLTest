b81590d18031d9a17f7c489aa41e72078e45a02a
Aditya Kumar <2020akumar@tjhsst.edu>
2019-10-01 11:35:56 -0400

    Added time callback

--- app/src/main/python/main.py
+++ app/src/main/python/main.py
@@ -5,12 +5,26 @@ from tensorflow.python.keras.datasets import mnist
 from tensorflow.python.keras.models import Sequential
 from tensorflow.python.keras.layers import Dense,Dropout,Activation,Flatten
 from tensorflow.python.keras.layers import Conv2D, MaxPooling2D
-
+from tensorflow.python.keras .callbacks import Callback
+import time
 import os
+class TimeStop(Callback):
+    def __init__(self,seconds=0):
+        super(Callback,self).__init__()
+        self.start_time=0
+        self.seconds=seconds
+
+    def on_train_begin(self, logs=None):
+        self.start_time=time.time()
+    def on_batch_end(self, batch, logs=None):
+        if time.time()-self.start_time>self.seconds:
+            self.model.stop_training=True
+            print("Stopped after %s seconds"%(self.seconds))
+
 def main():
     batch_size=32
     classes=10
-    epochs=1
+    epochs=5
     img_rows, img_cols = 28, 28
 
     (xtr,ytr),(xtst,ytst)=mnist.load_data()
@@ -53,8 +67,9 @@ def main():
     xtst=xtst.astype('float32')
     xtr/=255
     xtst/=255
-    
-    model.fit(xtr,ytr,batch_size=batch_size,epochs=epochs,validation_data=(xtst,ytst),shuffle=True)
+    second=int(input("How long"))
+    stopper=TimeStop(seconds=second)
+    model.fit(xtr,ytr,batch_size=batch_size,epochs=epochs,validation_data=(xtst,ytst),shuffle=True,callbacks=[stopper])
 
     
     scores=model.evaluate(xtst,ytst,verbose=1)
