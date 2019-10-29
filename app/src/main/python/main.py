170924fc5b6fc0465b5554f8fa04a1434d7faeaf
Aditya Kumar <2020akumar@tjhsst.edu>
2019-10-29 11:55:33 -0400

    my commit messages are really really really really really really bad

--- app/src/main/python/main.py
+++ app/src/main/python/main.py
@@ -27,68 +27,70 @@ class TimeStop(Callback):
 def main(second=150,conv=2,dens=2 ):
     print(tf.__version__)
     tf.reset_default_graph()
-    print(second)
-    batch_size=32
-    classes=10
-    epochs=5
-    img_rows, img_cols = 28, 28
+    graph=tf.get_default_graph()
+    with graph.as_default():
+        print(second)
+        batch_size=32
+        classes=10
+        epochs=5
+        img_rows, img_cols = 28, 28
 
-    (xtr,ytr),(xtst,ytst)=mnist.load_data()
+        (xtr,ytr),(xtst,ytst)=mnist.load_data()
 
-    xtr = xtr.reshape(xtr.shape[0], img_rows, img_cols, 1)
-    xtst = xtst.reshape(xtst.shape[0], img_rows, img_cols, 1)
-    input_shape = (img_rows, img_cols, 1)
+        xtr = xtr.reshape(xtr.shape[0], img_rows, img_cols, 1)
+        xtst = xtst.reshape(xtst.shape[0], img_rows, img_cols, 1)
+        input_shape = (img_rows, img_cols, 1)
 
-    ytr=keras.utils.to_categorical(ytr,classes)
-    ytst=keras.utils.to_categorical(ytst,classes)
+        ytr=keras.utils.to_categorical(ytr,classes)
+        ytst=keras.utils.to_categorical(ytst,classes)
 
-    model= Sequential()
-    model.add(Conv2D(32,(3,3),input_shape=input_shape))
-    model.add(Activation('relu'))
-    for aa in range(0,conv-1):
-        model.add(Conv2D(32,(3,3)))
+        model= Sequential()
+        model.add(Conv2D(32,(3,3),input_shape=input_shape))
         model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=(2,2)))
-    model.add(Dropout(0.25))
+        for aa in range(0,conv-1):
+            model.add(Conv2D(32,(3,3)))
+            model.add(Activation('relu'))
+        model.add(MaxPooling2D(pool_size=(2,2)))
+        model.add(Dropout(0.25))
 
-    # model.add(Conv2D(64,(3,3)))
-    # model.add(Activation('relu'))
-    # model.add(Conv2D(64,(3,3)))
-    # model.add(Activation('relu'))
-    # model.add(MaxPooling2D(pool_size=(2,2)))
-    # model.add(Dropout(0.25))
+        # model.add(Conv2D(64,(3,3)))
+        # model.add(Activation('relu'))
+        # model.add(Conv2D(64,(3,3)))
+        # model.add(Activation('relu'))
+        # model.add(MaxPooling2D(pool_size=(2,2)))
+        # model.add(Dropout(0.25))
 
-    model.add(Flatten())
-    for bb in range(1, dens):
-        model.add(Dense(512//bb))
-        model.add(Activation("relu"))
-    model.add(Dropout(0.5))
-    model.add(Dense(classes))
-    model.add(Activation('softmax'))
-    global numt
-    # opt=keras.optimizers.Adam(lr=.0001,decay=1e-7)
-    optimizers=["adam","nadam","adamax"]
-    model.compile(loss="categorical_crossentropy",optimizer=optimizers[numt%3],metrics=['accuracy'])
-    numt+=1
+        model.add(Flatten())
+        for bb in range(1, dens):
+            model.add(Dense(512//bb))
+            model.add(Activation("relu"))
+        model.add(Dropout(0.5))
+        model.add(Dense(classes))
+        model.add(Activation('softmax'))
+        global numt
+        # opt=keras.optimizers.Adam(lr=.0001,decay=1e-7)
+        optimizers=["adam","nadam","adamax"]
+        model.compile(loss="categorical_crossentropy",optimizer=optimizers[numt%3],metrics=['accuracy'])
+        numt+=1
 
-    xtr=xtr.astype('float32')
-    xtst=xtst.astype('float32')
-    xtr/=255
-    xtst/=255
+        xtr=xtr.astype('float32')
+        xtst=xtst.astype('float32')
+        xtr/=255
+        xtst/=255
 
-    stopper=TimeStop(seconds=second)
+        stopper=TimeStop(seconds=second)
 
-    model.fit(xtr,ytr,batch_size=batch_size,epochs=epochs,validation_data=(xtst,ytst),shuffle=True,callbacks=[stopper])
+        model.fit(xtr,ytr,batch_size=batch_size,epochs=epochs,validation_data=(xtst,ytst),shuffle=True,callbacks=[stopper])
 
-    scores=model.evaluate(xtst[0:1000],ytst[0:1000],verbose=1)
-    print("Loss:",scores[0] )
-    print("Accuracy:",scores[1])
-    del model
-    gc.collect()
+        scores=model.evaluate(xtst[0:1000],ytst[0:1000],verbose=1)
+        print("Loss:",scores[0] )
+        print("Accuracy:",scores[1])
+        del model
+        gc.collect()
 
-    return scores[1]
+        return scores[1]
 
 #
-# main(10, 2,2)
-# main(10,2,3)
+main(10, 2,2)
+main(10,2,3)
 
